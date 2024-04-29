2ec281bef8e60e4c470fde2d83e1984c2136138a
Matthew Blinow <mblinow@gmail.com>
2024-04-29 19:04:09 -0400

    More styling in place, need to work on Text

--- code/doThings.js
+++ code/doThings.js
@@ -1,6 +1,13 @@
-//const CSS = "hnmain: { border: 20px solid red; }";
 
-//insertCSS({code: CSS});
+
+let mainTable = document.getElementById("hnmain");
+
+mainTable.style.backgroundColor = "#121212";
+
+const tbody = mainTable.children[0];
+
+const header = tbody.children[0].children[0];
+header.style.background="#0f1242";
 
 document.body.style.background = "#535050";
-//document.body.style.border = "20px solid red";
+
